4fd01e3415dc9c17540edbea73f13fd6c38fa772
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2020-06-25 15:13:26 -0400

    Resolved merge conflict by incorporating both suggestions.

--- Donation-platform/target/portfolio-1/script.js
+++
@@ -1,8 +0,0 @@
-function getMessageUsingArrowFunctions()
-{
-    // Limit maximum commens shown
-    fetch('/Quickstart').then(response => response.json()).then((quote) =>
-    {
-        makeList(quote);
-    });
-};
