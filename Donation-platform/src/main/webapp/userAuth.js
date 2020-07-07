e639300f02b1c23436f60328cbf8bcbc446b5f91
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2020-07-07 18:31:02 -0400

    Revert "current commit"
    This reverts commit 18057d484ace7711f11f2baaa3b4741e84255485.

--- Donation-platform/src/main/webapp/userAuth.js
+++ Donation-platform/src/main/webapp/userAuth.js
@@ -3,6 +3,9 @@ function getUser()
     fetch('/userauth').then(response => response.json()).then((quote) =>
     {   
         console.log(quote)
+        if (quote["redirect"] == "true"){
+            location.href="/username"
+        }
         if (quote["loggedIn"] == "true"){
             document.getElementById('index-container').innerHTML = quote["logoutUrl"];
             document.getElementById('index-container').appendChild(document.createTextNode("Welcome: " + quote["username"]));
@@ -12,43 +15,3 @@ function getUser()
     }
     );
 }
-
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  var id_token = googleUser.getAuthResponse().id_token;
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', '/username');
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = function() {
-    console.log('Signed in as: ' + xhr.responseText);
-  };
-  xhr.send('idtoken=' + id_token);
-}
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
-
-function onLoad() {
-    gapi.load('auth2', function() {
-
-  gapi.auth2.init({
-
-    client_id: "223187457231-nspsjgjtsnpgjub4q12p37cdu134d6kk.apps.googleusercontent.com",
-
-  }).then(function(){
-
-    auth2 = gapi.auth2.getAuthInstance();
-    console.log(auth2.isSignedIn.get()); //now this always returns correctly        
-
-  });
-});
-}
-
