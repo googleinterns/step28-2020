94fa7eff93a118ef0bc12edc4b7fcddc66cd0764
Ryan Draper <ryandraper@google.com>
2020-07-29 22:28:24 +0000

    Create helper function for getting a charity's link.

--- charity-data/getcharities.py
+++ charity-data/getcharities.py
@@ -2,6 +2,21 @@ from urllib import parse
 import requests
 import json
 
+
+def get_charity_link(base_charity_url):
+    # Append / if not already present
+    if base_charity_url[len(base_charity_url) - 1] != '/':
+        base_charity_url += '/'
+    # If /donate is valid
+    if requests.get(base_charity_url + 'donate').status_code < 400:
+        return base_charity_url + 'donate'
+    # Otherwise, if homepage is valid
+    elif requests.get(base_charity_url).status_code < 400:
+        return base_charity_url
+    else:
+        return None
+
+
 base_url = 'https://api.data.charitynavigator.org/v2/Organizations?'
 
 # Parameters for request URL
@@ -21,7 +36,6 @@ charity_nav_json = requests.get(url).json()
 
 # Counters for the number of charities with no valid donate link, the number
 # with no valid homepage link, and the number with no valid image link.
-num_no_valid_donate = 0
 num_no_valid_link = 0
 num_no_image = 0
 
@@ -30,47 +44,33 @@ for charity in charity_nav_json:
     # Create a charity JSON object
     processed_charity = {}
     
-    # Name
+    # Add charity attributes to JSON object
     processed_charity['name'] = charity['charityName']
 
-    # Link
-    charity_url = charity['websiteURL']
-    # Append / if not already present
-    if charity_url[len(charity_url) - 1] != '/':
-        charity_url += '/'
     try:
-        # If /donate is valid
-        if requests.get(charity_url + 'donate').status_code < 400:
-            processed_charity['link'] = charity_url + 'donate'
-        # Otherwise, if homepage is valid
-        elif requests.get(charity_url).status_code < 400:
-            num_no_valid_donate += 1
-            processed_charity['link'] = charity_url
+        if get_charity_link(charity['websiteURL']) != None:
+            processed_charity['link'] = get_charity_link(charity['websiteURL'])
         else:
-            continue
+            num_no_valid_link += 1
     except requests.ConnectionError as e:
-        print("Failed to open url " + charity_url)
+        print("Failed to open url " + charity['websiteURL'])
         num_no_valid_link += 1
         continue
-    
-    # Image
+
     # NOTE: WE HAVE TO ATTRIBUTE CLEARBIT API:
     # <a href="https://clearbit.com">Logos provided by Clearbit</a>
     try:
-        if requests.get('https://logo.clearbit.com/' + charity_url).status_code < 400:
-            processed_charity['image'] = 'https://logo.clearbit.com/' + charity_url
+        if requests.get('https://logo.clearbit.com/' + charity['websiteURL']).status_code < 400:
+            processed_charity['image'] = 'https://logo.clearbit.com/' + charity['websiteURL']
         else:
             continue
     except requests.ConnectionError as e:
-        print("Failed to open url " + 'https://logo.clearbit.com/' + charity_url)
+        print("Failed to open url " + 'https://logo.clearbit.com/' + charity['websiteURL'])
         num_no_image += 1
         continue
 
-    # Category
     processed_charity['category'] = charity['category']['categoryName']
-    # Description
     processed_charity['description'] = charity['mission']
-    # Rating
     processed_charity['rating'] = charity['currentRating']['score']
     
     charities.append(processed_charity)
