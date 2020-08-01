2c60d72ca88f28efaa7216d2b98e0753a19b48c1
Ryan Draper <ryandraper@google.com>
2020-08-01 02:34:34 +0000

    Modularize code and check for 200 status codes.

--- charity-data/getcharities.py
+++ charity-data/getcharities.py
@@ -3,20 +3,6 @@ import requests
 import json
 
 
-def get_charity_link(base_charity_url):
-    # Append / if not already present
-    if base_charity_url[len(base_charity_url) - 1] != '/':
-        base_charity_url += '/'
-    # If /donate is valid
-    if requests.get(base_charity_url + 'donate').status_code < 400:
-        return base_charity_url + 'donate'
-    # Otherwise, if homepage is valid
-    elif requests.get(base_charity_url).status_code < 400:
-        return base_charity_url
-    else:
-        return None
-
-
 base_url = 'https://api.data.charitynavigator.org/v2/Organizations?'
 
 # Parameters for request URL
@@ -29,52 +15,97 @@ sort = 'RATING'
 params = {'app_id': app_id, 'app_key': app_key, 'pageSize': pageSize,
           'rated': rated, 'sizeRange': sizeRange, 'sort': sort}
 
-url = base_url + parse.urlencode(params)
 
-# Get JSON response body
-charity_nav_json = requests.get(url).json()
+def get_json(params):
+    url = base_url + parse.urlencode(params)
+    return requests.get(url).json()
 
-# Counters for the number of charities with no valid donate link, the number
-# with no valid homepage link, and the number with no valid image link.
-num_no_valid_link = 0
-num_no_image = 0
 
-charities = []
-for charity in charity_nav_json:
+def get_name(charity):
+    return charity['charityName']
+
+
+def get_link(base_charity_url):
+    # Append / if not already present
+    if base_charity_url[len(base_charity_url) - 1] != '/':
+        base_charity_url += '/'
+    # If /donate is valid
+    if requests.get(base_charity_url + 'donate').status_code == 200:
+        return base_charity_url + 'donate'
+    # Otherwise, if homepage is valid
+    elif requests.get(base_charity_url).status_code == 200:
+        return base_charity_url
+    else:
+        return None
+
+
+def get_image(charity):
+    return 'https://logo.clearbit.com/' + charity['websiteURL']
+
+
+def get_category(charity):
+    return charity['category']['categoryName']
+
+
+def get_description(charity):
+    return charity['mission']
+
+
+def get_rating(charity):
+    return charity['currentRating']['score']
+
+
+def process_charity(charity):
     # Create a charity JSON object
     processed_charity = {}
-    
-    # Add charity attributes to JSON object
-    processed_charity['name'] = charity['charityName']
 
+    # Add charity attributes to JSON object
+    processed_charity['name'] = get_name(charity)
+    
+    global num_no_valid_link
     try:
-        if get_charity_link(charity['websiteURL']) != None:
-            processed_charity['link'] = get_charity_link(charity['websiteURL'])
+        if get_link(charity['websiteURL']) is not None:
+            processed_charity['link'] = get_link(charity['websiteURL'])
         else:
             num_no_valid_link += 1
     except requests.ConnectionError as e:
         print("Failed to open url " + charity['websiteURL'])
         num_no_valid_link += 1
-        continue
+        return None
 
     # NOTE: WE HAVE TO ATTRIBUTE CLEARBIT API:
     # <a href="https://clearbit.com">Logos provided by Clearbit</a>
     try:
-        if requests.get('https://logo.clearbit.com/' + charity['websiteURL']).status_code < 400:
-            processed_charity['image'] = 'https://logo.clearbit.com/' + charity['websiteURL']
+        if requests.get(get_image(charity)).status_code == 200:
+            processed_charity['image'] = get_image(charity)
         else:
-            continue
+            return None
     except requests.ConnectionError as e:
-        print("Failed to open url " + 'https://logo.clearbit.com/' + charity['websiteURL'])
-        num_no_image += 1
-        continue
+        print("Failed to open url " + get_image(charity))
+        return None
 
-    processed_charity['category'] = charity['category']['categoryName']
-    processed_charity['description'] = charity['mission']
-    processed_charity['rating'] = charity['currentRating']['score']
-    
-    charities.append(processed_charity)
+    processed_charity['category'] = get_category(charity)
+    processed_charity['description'] = get_description(charity)
+    processed_charity['rating'] = get_rating(charity)
 
-# Populate charities.json file with charities
-with open("charities.json", "w") as outfile:
-    json.dump(charities, outfile)
+    return processed_charity
+
+
+if __name__ == "__main__":
+    # Counters for the number of charities without a valid donate or homepage link
+    global num_no_valid_link
+    num_no_valid_link = 0
+
+    # Get JSON response body
+    charity_nav_json = get_json(params)
+
+    # Build a list of charity JSON objects
+    charities = []
+    for charity in charity_nav_json:
+        processed_charity = process_charity(charity)
+        if processed_charity is not None:
+            charities.append(processed_charity)
+
+    # Populate charities.json file with charities
+    with open("charities.json", "w") as outfile:
+        json.dump(charities, outfile)
